a95fad5dfc78e72025b0aa8770a42430720278a7
miguel_paes <miguelpaesdasilva@gmail.com>
2025-05-05 16:01:26 -0300

    inicio do projeto

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HeaderComponent from './components/HeaderComponent.vue';
+</script>
+
+<template>
+  <header>
+    <HeaderComponent />
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,37 @@
+<script setup></script>
+
+<template>
+  <header>
+    <div class="left header-div">
+      <i class="icon bi bi-list"></i>
+    </div>
+
+    <div class="middle header-div">
+      <h1>Biological Pact</h1>
+    </div>
+
+    <div class="right header-div"></div>
+  </header>
+</template>
+
+<style scoped>
+  header {
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .icon {
+    font-size: 30px;
+  }
+
+  .header-div {
+    background-color: green;
+    padding: 1rem;
+    width: 32vw;
+  }
+
+  .middle {
+    text-align: center;
+  }
+</style>
